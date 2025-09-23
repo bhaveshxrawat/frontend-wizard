e4ddda0b169fc6848e95996807fbcbe115496dea
Bhavesh Rawat <rawatbhavesh2001@gmail.com>
2025-09-23 21:47:09 +0530

    home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,31 @@
+import "./home.css";
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <div className="">
-      <h2>Hlw</h2>
+    <div className="home">
+      <h1>Tasks</h1>
+      <div className="task _1">
+        <h2>
+          Task 1 - Design & UI {"("}CSS Battles{")"}
+        </h2>
+        <ul>
+          <li>
+            <Link href="/css_battle_1">#133 - Battle 1 ↗</Link>
+          </li>
+          <li>
+            <Link href="/css_battle_2">#239 - Battle 2 ↗</Link>
+          </li>
+        </ul>
+      </div>
+      <div className="task _2">
+        <h2>Task 2: Logic & Code Quality — Birthday Calendar</h2>
+        <ul>
+          <li>
+            <Link href="/birthday-calendar">Birthday Calendar ↗</Link>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
